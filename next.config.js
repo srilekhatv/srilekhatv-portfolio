097bd181d92e8cae70bf9e96de5bf18e6f770728
Srilekha Tirumala Vinjamoori <srilekha.tv@gmail.com>
2025-07-02 15:17:38 -0500

    Updated Certifications

--- next.config.js
+++ next.config.js
@@ -7,7 +7,25 @@ const nextConfig = {
       });
       return config;
     },
+
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.accredible.com',
+        port: '',
+        pathname: '/v1/frontend/credential_website_embed_image/badge/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.credly.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
   };
   
   module.exports = nextConfig;
-  
+
+
